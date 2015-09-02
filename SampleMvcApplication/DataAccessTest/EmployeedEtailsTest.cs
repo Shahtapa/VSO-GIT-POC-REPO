47e86cdd06a56df5483feb2dc202f9dc273dbabb
parvathymohan <parvathy.mohan@rapidvaluesolutions.com>
2015-09-02 12:39:59 +0530

    Commit To Git

--- SampleMvcApplication/DataAccessTest/EmployeedEtailsTest.cs
+++ SampleMvcApplication/DataAccessTest/EmployeedEtailsTest.cs
@@ -14,6 +14,7 @@ namespace DataAccessTest
         public void TestMethod1()
         {
 
+
          EmployeeDetailsController empController = new EmployeeDetailsController();
 
          XmlHelper xmlHelper = new XmlHelper();
