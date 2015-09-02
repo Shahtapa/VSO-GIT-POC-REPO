9856c05c294ef92a0a5c250253e06a405ac4224b
parvathymohan <parvathy.mohan@rapidvaluesolutions.com>
2015-09-02 12:30:45 +0530

    Commit

---
+++ SampleMvcApplication/SampleMvcApplication/Models/EmployeeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SampleMvcApplication.Models
+{
+    public class EmployeeViewModel
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeDesignation { get; set; }
+        public string EmployeeAddress { get; set; }
+    }
+}
