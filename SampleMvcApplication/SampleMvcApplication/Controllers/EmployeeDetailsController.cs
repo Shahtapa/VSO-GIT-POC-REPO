8416645c04b279b14e2726f5275bc699b55fdbb8
parvathymohan <parvathy.mohan@rapidvaluesolutions.com>
2015-09-03 15:12:44 +0530

    Commit

--- SampleMvcApplication/SampleMvcApplication/Controllers/EmployeeDetailsController.cs
+++ SampleMvcApplication/SampleMvcApplication/Controllers/EmployeeDetailsController.cs
@@ -12,7 +12,7 @@ namespace SampleMvcApplication.Controllers
 {
     public class EmployeeDetailsController : Controller
     {
-        //
+       //
         // GET: /EmployeeDetails/
 
         public ActionResult Index()
