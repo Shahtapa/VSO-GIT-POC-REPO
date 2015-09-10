1ed6012703b077e023ca9e07e0927b457e920691
parvathymohan <parvathy.mohan@rapidvaluesolutions.com>
2015-09-10 15:51:34 +0530

    Commit

--- SampleMvcApplication/SampleMvcApplication/BatchScript/GitScript.bat
+++ SampleMvcApplication/SampleMvcApplication/BatchScript/GitScript.bat
@@ -1,7 +1,9 @@
 
 c:\Windows\System32\cmd.exe /C "tf merge Devteam-A integration /recursive"
+tf merge Devteam-A integration /recursive
 
-REM git checkout integration
+
+git checkout integration
 REM git pull origin integration
 REM git merge Devteam-A
 REM git merge Devteam-B 
