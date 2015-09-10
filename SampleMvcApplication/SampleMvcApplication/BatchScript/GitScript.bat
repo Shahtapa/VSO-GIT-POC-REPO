f2f67f168257f5c24da475f56630306692acdb5a
parvathymohan <parvathy.mohan@rapidvaluesolutions.com>
2015-09-10 17:05:07 +0530

    C

--- SampleMvcApplication/SampleMvcApplication/BatchScript/GitScript.bat
+++ SampleMvcApplication/SampleMvcApplication/BatchScript/GitScript.bat
@@ -4,5 +4,6 @@ git config user.email "parvathy.mohan@rapidvaluesolutions.com"
 git merge origin/devteam-A
 git merge origin/devteam-B 
 git config push.default simple
+git push origin HEAD:integration
 git push 
 
