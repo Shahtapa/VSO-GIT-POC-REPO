08fdce275189767818aac2d25d140d1b2b04aef5
parvathymohan <parvathy.mohan@rapidvaluesolutions.com>
2015-09-10 16:34:34 +0530

    Commit

--- SampleMvcApplication/SampleMvcApplication/BatchScript/GitScript.bat
+++ SampleMvcApplication/SampleMvcApplication/BatchScript/GitScript.bat
@@ -1,6 +1,6 @@
 
-git config --global user.name "parvathymohan"
-git config --global user.email "parvathy.mohan@rapidvaluesolutions.com"
+git config user.name "parvathymohan"
+git config user.email "parvathy.mohan@rapidvaluesolutions.com"
 git merge origin/devteam-A
 git merge origin/devteam-B 
 git push origin inegration
