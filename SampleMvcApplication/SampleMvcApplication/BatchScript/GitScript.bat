d55a45808c31b5ec49ef81a52e341ac00d8d68e5
shahtapa <shahtapa@gmail.com>
2015-09-14 11:12:24 +0530

    Commit

--- SampleMvcApplication/SampleMvcApplication/BatchScript/GitScript.bat
+++ SampleMvcApplication/SampleMvcApplication/BatchScript/GitScript.bat
@@ -1,4 +1,6 @@
 git remote add origin https://github.com/Shahtapa/vso-git-poc-repo.git
+git config  user.email "parvathy.mohan@rapidvaluesolutions.com"
+git config user.name "parvathymohan"
 git merge origin/devteam-A
 git commit -m "commit message"
 git push -u origin --all
