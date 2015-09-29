5bf098f4eb6e7eaf725aeaf0176f1d96f0e6391b
parvathymohan <parvathy.mohan@rapidvaluesolutions.com>
2015-09-29 15:00:59 +0530

    Update GitBatch.bat

--- SampleMvcApplication/SampleMvcApplication/BatchScript/GitBatch.bat
+++ SampleMvcApplication/SampleMvcApplication/BatchScript/GitBatch.bat
@@ -1,7 +1,7 @@
 
 git remote set-url origin https://parvathymohan:parvathymohan1@github.com/Shahtapa/vso-git-poc-repo.git
 
-git merge origin/devteam-C
-git merge origin/devteam-D
-git push -u origin master
+git merge devteam-C
+git merge devteam-D
+git push -u  master
 
