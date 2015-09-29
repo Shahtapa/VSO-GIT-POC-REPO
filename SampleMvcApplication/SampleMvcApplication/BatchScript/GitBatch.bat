13b1b352828d029ecd72764a335b6d77c816ba56
parvathymohan <parvathy.mohan@rapidvaluesolutions.com>
2015-09-29 15:59:48 +0530

    C

--- SampleMvcApplication/SampleMvcApplication/BatchScript/GitBatch.bat
+++ SampleMvcApplication/SampleMvcApplication/BatchScript/GitBatch.bat
@@ -1,7 +1,7 @@
 git config --global user.email "parvathy.mohan@rapidvaluesolutions.com"
   git config --global user.name "parvathymohan"
  git remote set-url origin https://parvathymohan:parvathymohan1@github.com/Shahtapa/vso-git-poc-repo.git
- git merge origin/devteam-C
-git merge origin/devteam-D
+ git merge  origin/devteam-C integration
+git merge origin/devteam-D integration
 git checkout integration
 git push origin  integration
